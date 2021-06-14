200967f837fb513c7f7bc41b2b850e6933ff51bb
Mina <MHCSC01@GMAIL.COM>
2021-06-14 12:12:01 -0500

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include "hello_functions.h"
 
-int main() {
-   printf("Hello, World!");
+int main(void){
+   printf("Hello World\n");
+   int x = 2;
+   int y = 3;
+   int z = add(x, y);
 }
